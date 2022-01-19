00908097deb896b42b1de44b902fe2090ffe117f
junho123 <wn531300@gmail.com>
2022-01-19 14:50:08 +0900

    add remark

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
+'use strict';
+
 // 아이템들을 json으로 변환하여 가져오기
 function loadItems() {
-  return fetch("../data/data.json")
+  return fetch('data/data.json')
     .then((response) => response.json())
     .then((json) => json.items);
 }
 
-// 화면에 보여줌
+// 화면에 아이템 보여줌
 function displayItems(items) {
-  const container = document.querySelector(".items");
-  container.innerHTML = items.map((item) => createHTMLString(item)).join("");
+  const container = document.querySelector('.items');
+  container.innerHTML = items.map((item) => createHTMLString(item)).join('');
 }
 
 // 데이터를 HTML 형식으로 만듦
@@ -23,13 +25,13 @@ function createHTMLString(item) {
 
 // 이벤트 정의
 function setEventListeners(items) {
-  const logo = document.querySelector(".logo");
-  const buttons = document.querySelector(".buttons");
-  logo.addEventListener("click", () => displayItems(items));
-  buttons.addEventListener("click", (event) => onButtonClick(event, items));
+  const logo = document.querySelector('.logo');
+  const buttons = document.querySelector('.buttons');
+  logo.addEventListener('click', () => displayItems(items));
+  buttons.addEventListener('click', (event) => onButtonClick(event, items));
 }
 
-// 버튼 클릭시
+// 버튼 클릭시 발생
 function onButtonClick(event, items) {
   const dataset = event.target.dataset;
   const key = dataset.key;
